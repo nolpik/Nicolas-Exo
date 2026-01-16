5c257583c2be89c5e75a14ed64a9e0444fed57b3
GUILLOT-Tristan <t.guillot@game-sup.fr>
2026-01-16 16:20:12 +0100

    Add Script TheRat

---
+++ Assets/TristanTheRat.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class TristanTheRat : MonoBehaviour
+{
+    [SerializeField] private int fatigue = 5;
+    public bool hasFatigue;
+    SerializeField private int Cheese = 1
+    public bool hasCheese;
+}
+
